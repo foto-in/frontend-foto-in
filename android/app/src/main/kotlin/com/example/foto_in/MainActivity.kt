0fd08bccab9069cc6e09d79db65a427137657594
Falah <falah@Falahs-MacBook-Air.local>
2024-03-16 14:53:32 +0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/foto_in/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.foto_in
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/styles/typography.dart
@@ -0,0 +1,218 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FotoInHeadingTypography {
+  const FotoInHeadingTypography(this.color);
+  final Color color;
+
+  get textColor => color;
+  static TextStyle megaLarge({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 64,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: -0.02,
+        color: color,
+      );
+
+  static TextStyle mega({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 48,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: -0.02,
+        color: color,
+      );
+
+  static TextStyle xxLarge({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 40,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: -0.02,
+        color: color,
+      );
+
+  static TextStyle xLarge({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 36,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: -0.02,
+        color: color,
+      );
+
+  static TextStyle large({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 32,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: -0.02,
+        color: color,
+      );
+
+  static TextStyle medium({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 28,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: -0.02,
+        color: color,
+      );
+
+  static TextStyle small({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: -0.02,
+        color: color,
+      );
+
+  static TextStyle xSmall({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: -0.02,
+        color: color,
+      );
+
+  static TextStyle xxSmall({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: -0.02,
+        color: color,
+      );
+}
+
+class FotoInSubHeadingTypography {
+  const FotoInSubHeadingTypography(this.color);
+  final Color color;
+
+  get textColor => color;
+  static TextStyle mega({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 48,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle xxLarge({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle xLarge({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle large({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 18,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle medium({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle small({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 14,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle xSmall({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 12,
+        fontWeight: FontWeight.w700,
+        height: 1.2,
+        letterSpacing: 0,
+        color: color,
+      );
+}
+
+class FotoInLabelTypography {
+  const FotoInLabelTypography(this.color);
+  final Color color;
+
+  get textColor => color;
+  static TextStyle large({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 18,
+        fontWeight: FontWeight.w500,
+        height: 1.5,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle medium({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        height: 1.5,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle small({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        height: 1.5,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle xSmall({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+        height: 1.5,
+        letterSpacing: 0,
+        color: color,
+      );
+}
+
+class FotoInParagraph {
+  const FotoInParagraph(this.color);
+  final Color color;
+
+  get textColor => color;
+  static TextStyle large({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 18,
+        fontWeight: FontWeight.w400,
+        height: 1.5,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle medium({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        height: 1.5,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle small({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        height: 1.5,
+        letterSpacing: 0,
+        color: color,
+      );
+
+  static TextStyle xSmall({Color? color}) => GoogleFonts.plusJakartaSans(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        height: 1.5,
+        letterSpacing: 0,
+        color: color,
+      );
+}
